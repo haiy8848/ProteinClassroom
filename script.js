c02daec6f5f25cb070979b38a71a855f94d545db
Yan, Hai <hai.yan@hotmail.com>
2024-04-12 18:12:58 +0800

    add chat page

---
+++ script.js
@@ -0,0 +1,24 @@
+
+const messageInput = document.getElementById('message-input');
+const submitBtn = document.getElementById('submit-btn');
+const chatDisplay = document.getElementById('.chat-display');
+
+
+submitBtn.addEventListener('click', sendMessage);
+
+function sendMessage() {
+  const message = messageInput.value;
+
+  const newMessage = document.createElement('div');
+  newMessage.classList.add('message');
+
+  newMessage.innerHTML = message;
+  newMessage.style.backgroundColor = '#428bca';
+  newMessage.style.color = 'white';
+  newMessage.style.padding = '5px 10px';
+  newMessage.style.marginBottom = '10px';
+
+  chatDisplay.appendChild(newMessage);
+
+  messageInput.value = '';
+}
