9adf7ec2865dbcbbd10aaeac51739f63f6cd0717
ark <arkiner@hotmail.com>
2024-04-13 16:06:25 +0800

    use different color to show user message

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const messageInput = document.getElementById('message-input');
 const submitBtn = document.getElementById('submit-btn');
 const chatDisplay = document.querySelector('.chat-display');
 
+const username = 'Student';
+
 
 submitBtn.addEventListener('click', sendMessage);
 
@@ -10,13 +12,19 @@ function sendMessage() {
   const message = messageInput.value;
 
   const newMessage = document.createElement('div');
+ 
   newMessage.classList.add('message');
 
   newMessage.innerHTML = message;
-  newMessage.style.backgroundColor = '#428bca';
-  newMessage.style.color = 'white';
   newMessage.style.padding = '5px 10px';
   newMessage.style.marginBottom = '10px';
+  if (username == 'Student') {
+	  newMessage.style.backgroundColor = '#428bca';
+	  newMessage.style.color = 'white';
+  } else {
+	  newMessage.style.backgroundColor = '#f5f5f5';
+	  newMessage.style.color = '#333';
+  }
 
   chatDisplay.appendChild(newMessage);
 
